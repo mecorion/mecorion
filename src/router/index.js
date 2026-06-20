a48ac5a9ea4e67218be2459cbe1740fa4650dd58
Mecorion <mecorion@atomicmail.io>
2026-06-21 02:31:30 +0300

    Добавлены новые пути для музыкального сервиса и Dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,17 @@ import VideoPage from "../pages/VideoPage.vue";
 import HomePage from "@/pages/HomePage.vue";
 import PlayerPage from "@/pages/PlayerPage.vue";
 import UiKitView from "@/pages/UiKitView.vue";
+import DashboardView from "@/pages/DashboardView.vue";
+import MusicView from "@/pages/MusicView.vue";
 
 const routes = [
-    {path: "/home", alias: '/', component: HomePage},
-    {path: "/videos/:category", component: VideoPage, props: true},
-    {path: "/watch", component: PlayerPage},
-    {path: "/watch/:videoUrl", component: PlayerPage, props: true},
+    {path: "/", component: DashboardView, meta: {workspace: true}},
+    {path: "/dashboard", component: DashboardView, meta: {workspace: true}},
+    {path: "/music", component: MusicView, meta: {workspace: true}},
+    {path: "/home", component: HomePage, meta: {needsContent: true}},
+    {path: "/videos/:category", component: VideoPage, props: true, meta: {needsContent: true}},
+    {path: "/watch", component: PlayerPage, meta: {needsContent: true}},
+    {path: "/watch/:videoUrl", component: PlayerPage, props: true, meta: {needsContent: true}},
     {path: "/ui-kit", component: UiKitView},
 ];
 
